8ae52221e0e2655b0f13d68b460526a420cf3466
yangjiewu <wuyangjie89@gmail.com>
2016-01-19 11:03:18 +0800

    yangjiewu 0119 11:03
    yangjiewu 0119 11:03

---
+++ js/common.js
@@ -0,0 +1,61 @@
+// core: me
+; (function(window, $) {
+    window.me = {};
+})(window, window.jQuery || window.Zepto);
+
+// host
+; (function(me, $) {
+	var env = "mock"; // 测试环境与正式环境切换开关
+	var host = function(name) {
+        var url = {
+            mock: {
+                demoTest: './mock/demoTest.json'
+            },
+            pro: {
+                demoTest: 'http://xxx.xxx.xxx/xxxxx'
+            }
+        };
+        return url[env][name] || null;
+    };
+    me.host = host;
+})(me, window.jQuery || window.Zepto);
+
+// layout
+; (function(me, $) {
+    var Layout = function() {};
+    Layout.prototype.header = function() {
+        
+    };
+    Layout.prototype.menu = function() {
+        this.menuToggle("parent_01");
+        this.menuToggle("parent_02");
+    };
+    Layout.prototype.menuToggle = function(id, callback) {
+        $("#" + id).on("click", function() {
+            if ($("." + id).css("display") == "block") {
+                $("." + id).hide();
+            } else {
+                $("." + id).show();
+            };
+            if (callback) {
+                callback();
+            };
+        });
+    };
+    me.layout = new Layout();
+})(me, window.jQuery || window.Zepto);
+
+// util
+; (function(me, $) {
+    var Util = function() {};
+    Util.prototype.layout = function() {
+        $("#header").load("layout/header.html", {}, function() {
+            me.layout.header();
+        });
+        $("#menu").load("layout/menu.html", {}, function() {
+            me.layout.menu();
+        });
+        $("#footer").load("layout/footer.html");
+    };
+    me.util = new Util();
+})(me, window.jQuery || window.Zepto);
---
+++ js/demo.js
@@ -0,0 +1,19 @@
+$(function() {
+	// 加载页面公共部分
+	me.util.layout();
+
+	// ajax示例
+	$.ajax({
+        type: "GET",
+        url: me.host("demoTest"),
+        dataType: "json",
+        data: {arg01: "xxx"},
+        error: function(res) {
+            alert("ajax error!");
+        },
+        success: function(res) {
+            console.log(res);
+        }
+    });
+
+});
