7f3271e051b655fe2e66609deaea20e27b3a41a3
yangjiewu <wuyangjie89@gmail.com>
2016-01-20 15:44:18 +0800

    yangjiewu floorMap2D 单选框

--- js/floorMap2D.js
+++ js/floorMap2D.js
@@ -1,6 +1,6 @@
 $(function() {
 	// 加载页面公共部分
-	me.util.layout();
+	// me.util.layout();
 
     // 初始化巡检点
 	$.ajax({
@@ -12,41 +12,37 @@ $(function() {
             alert("ajax error!");
         },
         success: function(res) {
-            var width = parseFloat($("#map").css("width"));
-            var height = parseFloat($("#map").css("height"));
+            var width = parseFloat($(".map").css("width"));
+            var height = parseFloat($(".map").css("height"));
             var equipmentArr = res.equipmentPoint;
             var inspectArr = res.inspectPoint;
             console.log(equipmentArr);
             equipmentArr.forEach(function(obj) {
-                $("#map").append('' +
-                    '<div class="state normal" style="top:' + (obj.y * height - 5) + 'px;left:' + (obj.x * width - 5) + 'px;" floor="' + obj.floor + '" type="' + obj.type + '" name="' + obj.name + '" ></div>' + 
-                    '<div class="state alert" style="top:' + (obj.y * height - 5) + 'px;left:' + (obj.x * width - 5) + 'px;" floor="' + obj.floor + '" type="' + obj.type + '" name="' + obj.name + '" ></div>'
+                $(".map").append('' +
+                    '<div class="state normal" style="top:' + (obj.y * height - 5) + 'px;left:' + (obj.x * width - 5) + 'px;" floor="' + obj.floor + '" type="' + obj.type + '" name="' + obj.name + '" ></div>'
                 );
             });
             console.log(inspectArr);
             inspectArr.forEach(function(obj) {
-                $("#map").append('' +
-                    '<div class="state normal" style="top:' + (obj.y * height - 5) + 'px;left:' + (obj.x * width - 5) + 'px;" floor="' + obj.floor + '" stat="' + obj.stat + '" name="' + obj.name + '" ></div>' + 
-                    '<div class="state alert" style="top:' + (obj.y * height - 5) + 'px;left:' + (obj.x * width - 5) + 'px;" floor="' + obj.floor + '" stat="' + obj.stat + '" name="' + obj.name + '" ></div>'
+                $(".map").append('' +
+                    '<div class="state normal" style="top:' + (obj.y * height - 5) + 'px;left:' + (obj.x * width - 5) + 'px;" floor="' + obj.floor + '" stat="' + obj.stat + '" name="' + obj.name + '" ></div>'
                 );
             });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            bindEvent();
         }
     });
+
+    function bindEvent() {
+        // 左侧checkbox事件
+        $(".checkbox-item").on("change", function() {
+            $(".state").hide();
+            var all = document.getElementsByClassName("checkbox-item");
+            for (var i = 0; i < all.length; i++) {
+                if (all[i].checked == true) {
+                   $(".map").find("div[type='" + all[i].value + "']").show();
+                };
+            };
+        });
+    };
 
 });
