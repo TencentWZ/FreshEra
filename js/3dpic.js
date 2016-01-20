9603509f4858b6c9a3d6317ff81e104ee26d6974
Tomc <chizhang198908@gmail.com>
2016-01-21 00:06:55 +0800

    完成3d选择功能，点可以随着旋转

---
+++ js/3dpic.js
@@ -0,0 +1,50 @@
+// 定义图片组
+var frames = SpriteSpin.sourceArray('css/images/3d_{frame}.jpg', {
+    frame: [1, 24],
+    digits: 2
+});
+
+// 要停留的图片
+var details = [0, 20];
+var detailIndex = 0;
+
+// 初始化
+var spin = $('#spritespin');
+spin.spritespin({
+    source: frames,
+    width: 480,
+    sense: -1,
+    height: 480,
+    animate: false
+});
+// 得到api
+var api = spin.spritespin("api");
+
+spin.bind("onLoad", function(){
+    var data = api.data;
+    data.stage.prepend($(".details .detail"));
+    data.stage.find(".detail").hide();         
+}).bind("onFrame", function(){
+    var data = api.data;
+    data.stage.find(".detail:visible").stop(false).fadeOut();
+    data.stage.find(".detail.detail-" + data.frame).stop(false).fadeIn();
+});
+
+$( "#prev" ).click(function(){
+    setDetailIndex(detailIndex - 1);
+});
+
+$( "#next" ).click(function(){
+    setDetailIndex(detailIndex + 1);
+});
+
+function setDetailIndex(index){
+    detailIndex = index;
+    if (detailIndex < 0){
+        detailIndex = details.length - 1;
+    }
+    if (detailIndex >= details.length){
+        detailIndex = 0;
+    }
+    api.playTo(details[detailIndex]);
+}
