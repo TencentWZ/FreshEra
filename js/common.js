dcbe9079f8457cadb105e010f05e0e52ab5ad177
yangjiewu <wuyangjie89@gmail.com>
2016-02-01 15:00:39 +0800

    恢复周六代码

--- js/common.js
+++ js/common.js
@@ -11,11 +11,9 @@
 	var host = function ( name ) {
 		var url = {
 			mock: {
-				demoTest: './mock/demoTest.json?v=' + Math.random(),
 				floorMapInit: './mock/floorMapInit.json?v=' + Math.random(),
 				floorMapMonitor: './mock/floorMapMonitor.json?v=' + Math.random(),
 				floorMapBasic: './mock/floorMapBasic.json?v=' + Math.random(),
-				untreatedAlarm: './mock/untreatedAlarm.json?v=' + Math.random(),
 				patrolState: './mock/patrolState.json?v=' + Math.random(),
 				alarmHistory: './mock/alarmHistory.json?v=' + Math.random(),
 				alarmDeal: './mock/alarmDeal.json?v=' + Math.random(),
@@ -26,14 +24,13 @@
 				floor3DMapInit: './mock/floor3DMapInit.json?v=' + Math.random()
 			},
 			pro: {
-				demoTest: 'http://xxx.xxx.xxx/xxxxx?v=' + Math.random(),
-				floorMapInit: 'http://xxx.xxx.xxx/xxxxx?v=' + Math.random(),
-				floorMapMonitor: 'http://xxx.xxx.xxx/xxxxx?v=' + Math.random(),
-				floorMapBasic: 'http://xxx.xxx.xxx/xxxxx?v=' + Math.random(),
-				untreatedAlarm: 'http://xxx.xxx.xxx/xxxxx?v=' + Math.random(),
-				patrolState: 'http://xxx.xxx.xxx/xxxxx?v=' + Math.random(),
-				alarmHistory: 'http://xxx.xxx.xxx/xxxxx?v=' + Math.random(),
-				alarmDeal: 'http://xxx.xxx.xxx/xxxxx?v=' + Math.random(),
+				floorMapInit: 'accounts/floorMapInit.aspx?bid=151104001&floor_id=F1&picType=平面图',
+ 				floorMapMonitor: 'accounts/floorMapMonitor.aspx?building_id=151104001&floor_id=F1',
+ 				floorMapBasic: 'accounts/floorMapBasic.aspx?bid=151104001',
+ 				patrolState: 'accounts/patrolState.aspx?building_id=151104001',
+ 				alarmHistory: 'accounts/alarmDeals.aspx?building_id=151104001',
+ 				alarmDealInit: 'accounts/alarmDealInit.aspx?building_id=151104001&alarm_id=1',
+ 				alarmDeal: 'accounts/alarmDeal.aspx?building_id=151104001&alarm_id=1&deal_type=误报',
 				login: 'accounts/login.aspx',
 				management: 'accounts/management.aspx',
 				baseData: 'accounts/infoall.aspx',
@@ -187,16 +184,21 @@
 ( function ( me, $ ) {
 	var Util = function () {};
 	Util.prototype.layout = function () {
-		$( "#header" ).load( "layout/header.html", {}, function () {
-			me.layout.header();
-		} );
-		$( "#search-data").load( "layout/search.html", {}, function() {
-			me.layout.search();
-		});
-		$( "#menu" ).load( "layout/menu.html", {}, function () {
-			me.layout.menu();
-		} );
-		$( "#footer" ).load( "layout/footer.html" );
+		$.get("./layout/header.html", function (result) {
+ 			$( "#header" ).append(result);
+  			me.layout.header();
+  		} );
+		$.get("./layout/search.html", function (result) {
+ 			$( "#search-data" ).append(result);
+  			me.layout.search();
+ 		} );
+		$.get("./layout/menu.html", function (result) {
+ 			$( "#menu" ).append(result);
+  			me.layout.menu();
+  		} );
+		$.get("./layout/footer.html", function (result) {
+ 			$( "#footer" ).append(result);
+ 		} );
 	};
 	me.util = new Util();
 } )( me, window.jQuery || window.Zepto );
